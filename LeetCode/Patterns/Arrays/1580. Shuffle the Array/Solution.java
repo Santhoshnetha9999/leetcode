9163d3605b7be7571772fbe4f9f144bcf32a6484
Gaddam Santhosh Netha <gaddamsanthosh895@gmail.com>
2026-08-15 22:30:21 +0530

    Add Java Solution to Patterns/Arrays

---
+++ LeetCode/Patterns/Arrays/1580. Shuffle the Array/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        int[] shuffle =new int[2*n];
+       
+        int p1=0;
+        int p2 =n;
+
+        for(int i=0;i<2*n;i++){
+            if(i%2==0){
+                shuffle[i]=nums[p1];
+                p1++;
+            }else {
+                shuffle[i]=nums[p2];
+                p2++;
+            }
+        }
+
+     return shuffle;
+
+    }
+}
